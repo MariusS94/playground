77fda6d437a47d87653df578e08622c02800c4e6
MariusS94 <marius1spomer@gmail.com>
2020-08-05 16:17:44 +0200

    Add image import

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { createTitle } from "./components/title.js";
 import { createToggleButton } from "./components/toggleButton.js";
 import "./components/toggleButton.css";
 import "./global.css";
+import img from "../src/assets/lap.jpg";
 
 const title = createTitle();
 const toggleButton = createToggleButton();
